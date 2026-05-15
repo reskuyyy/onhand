cf658b1c79adcfc91ddefb40ab2ab2a51777b2ad
reskuyyy <reskygilang07@gmail.com>
2026-05-15 13:52:19 +0700

    Update onhand.js

--- api/onhand.js
+++ api/onhand.js
@@ -4,33 +4,39 @@ export default async function handler(req, res) {
 
   try {
 
+    // endpoint yang sebelumnya WORK
     const url =
-      `https://app.alfastore.co.id/prd/api/mob/tablet/productinfo/CheckPerRack/?storeId=${storeId}&plu=${plus}`;
+      `https://app.alfastore.co.id/prd/api/cex/item_by_rak/?storeId=${storeId}&rack=AU1`;
 
     const response = await fetch(url);
 
     const data = await response.json();
 
-    const result = [];
+    // filter berdasarkan PLU
+    const plusArray = String(plus)
+      .split(',')
+      .map(x => x.trim());
+
+    let filtered = [];
 
     if (Array.isArray(data)) {
 
-      data.forEach(item => {
-
-        result.push({
+      filtered = data
+        .filter(item =>
+          plusArray.includes(String(item.plu))
+        )
+        .map(item => ({
           barcode: item.barcode || '-',
           plu: item.plu || '-',
           nama: item.descp || '-',
           on_hand: item.onhand || 0
-        });
-
-      });
+        }));
 
     }
 
     res.status(200).json({
       status: true,
-      data: result
+      data: filtered
     });
 
   } catch (err) {
