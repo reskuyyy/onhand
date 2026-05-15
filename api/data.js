95a3b1e12d3f11b0829b28880d956d117bce6d58
reskuyyy <reskygilang07@gmail.com>
2026-05-15 13:15:45 +0700

    Update data.js

--- api/data.js
+++ api/data.js
@@ -1,15 +1,25 @@
 export default async function handler(req, res) {
-  const { storeId, rack } = req.query;
+
+  const { storeId } = req.query;
 
   try {
-    const response = await fetch(
-      `https://app.alfastore.co.id/prd/api/cex/item_by_rak/?storeId=${storeId}&rack=${rack}`
-    );
+
+    // sementara hardcode rack dulu
+    const rack = 'AU1';
+
+    const url =
+      `https://app.alfastore.co.id/prd/api/cex/item_by_rak/?storeId=${storeId}&rack=${rack}`;
+
+    const response = await fetch(url);
 
     const data = await response.json();
 
     res.status(200).json(data);
+
   } catch (err) {
-    res.status(500).json({ error: err.message });
+
+    res.status(500).json({
+      error: err.message
+    });
   }
 }
